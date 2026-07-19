d7b454fb50663cc07fcac9fd8b6f6e5520064d0a
sujay-777 <sujaysudharshan7@gmail.com>
2026-07-19 13:04:35 +0530

    Add Spring Boot application entry point

---
+++ src/main/java/com/cdc/mutation_tracker/MutationTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.cdc.mutation_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MutationTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MutationTrackerApplication.class, args);
+	}
+
+}
